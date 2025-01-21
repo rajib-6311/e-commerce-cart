7b2527c4acbb9c609562f38ec3c45e875aa7b015
rajibhossen <mhossen6311@gmail.com>
2025-01-22 01:53:27 +0600

    Update all files

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,11 +2,13 @@
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import search_icon from './search_icon.png'
+import cross_icon from './cross_icon.png'
 
 
 export const assets = {
     cart_icon,
     search_icon,
     bin_icon,
+    cross_icon
 }
 
